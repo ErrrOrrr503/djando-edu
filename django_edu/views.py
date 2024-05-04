a005f7dffb4dfd79c764787d7733007658769d5e
errrorrr503 <egortit-33@yandex.ru>
2024-05-04 13:34:59 +0300

    Corrected gitignore, added mypy typing.

--- django_edu/views.py
+++ django_edu/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.core.cache import cache
+from django.http import HttpRequest, HttpResponse
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
 
 g_tasks = [ "case1", "case2" ]
-def tasks(request, task_num = 0):
+def tasks(request: HttpRequest, task_num: int = 0) -> HttpResponse:
     context = {}
     if not request.session.get('task_num'):
         request.session['task_num'] = 1
     if task_num != 0:
         request.session['task_num'] = task_num
-    cur_active = request.session.get('task_num')
+    cur_active = request.session.get('task_num') or 1
     context["task_active_list"] = [ "active" if cur_active == (task_idx + 1) else "" for task_idx in range(len(g_tasks)) ]
-    context["task_name"] = g_tasks[cur_active - 1]
+    context["task_name"] = [g_tasks[cur_active - 1]]
     return render(request, "tasks.html", context=context)
